4c88fc8427126cdd31c7769f5007cbdf083a53f0
Ammar Alhasan <a.hasan@ligadata.com>
2021-03-25 00:51:03 +0400

    install react router and put base routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <h2>Header</h2>
+
+                <ul>
+                    <li>
+                        <Link to="/new">new list screen</Link>
+                    </li>
+                    <li>
+                        <Link to="/past">past list screen</Link>
+                    </li>
+                    <li>
+                        <Link to="/past/12">past item</Link>
+                    </li>
+                    <li>
+                        <Link to="/new/25">new item</Link>
+                    </li>
+                </ul>
+
+                <Switch>
+                    <Route path="/:type/:id" children={<ArticleItem/>}/>
+                    <Route path="/:type" children={<ArticlesList/>}/>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+function ArticlesList() {
+    let params: any = useParams();
+    let {type} = params
+    return (
+        <div>
+            <h3>All articles for: {type}</h3>
+        </div>
+    );
+}
+
+function ArticleItem() {
+    let params: any = useParams();
+    let {type, id} = params
+    return (
+        <div>
+            <h3>the type for this article is {type} and the id is : {id}</h3>
+        </div>
+    );
 }
 
 export default App;
