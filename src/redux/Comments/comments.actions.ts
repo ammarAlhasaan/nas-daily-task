a0dc86ed189d2ca446a0309d43f2d7ea31f411b5
Ammar Alhasan <a.hasan@ligadata.com>
2021-03-26 08:54:26 +0400

    cleanup

--- src/redux/Comments/comments.actions.ts
+++ src/redux/Comments/comments.actions.ts
@@ -2,20 +2,12 @@ import {
     CLEAR_COMMENTS,
     GET_COMMENTS,
     GET_COMMENT,
-    LOAD_MORE,
-    SELECT_COMMENT,
 } from './comments.types';
 import hackerNewsApi from "../../apis/hacker-news.api";
 
 
-export const selectComment = (comment: any) => {
-    return {type: SELECT_COMMENT, payload: comment};
-}
 export const clearComments = () => {
     return {type: CLEAR_COMMENTS};
-}
-export const loadMore = () => {
-    return {type: LOAD_MORE};
 }
 export const getComment = (id: string) => async (dispatch: any) => {
     const response = await hackerNewsApi.get(`/item/${id}.json`);
--- src/redux/Comments/comments.reducer.ts
+++ src/redux/Comments/comments.reducer.ts
@@ -3,19 +3,11 @@ import {
     SELECT_COMMENT,
     GET_COMMENT,
     CLEAR_COMMENTS,
-    LOAD_MORE, GET_REPLIES, GET_REPLY
 } from "./comments.types";
 
-const PAGE_SIZE = 10
 const INITIAL_STATE = {
     comments: [],
     commentsIds: [],
-    replies: [],
-    repliesIds: [],
-    currentCommentsIds: [],
-    selectedComment: {},
-    selectedType: '',
-    pageSize: 0,
 };
 const reducer = (state = INITIAL_STATE, action: any) => {
     switch (action.type) {
@@ -34,26 +26,6 @@ const reducer = (state = INITIAL_STATE, action: any) => {
         case SELECT_COMMENT:
             return {
                 ...state, selectedComment: action.payload,
-            };
-
-
-        case GET_REPLIES:
-            return {
-                ...state, repliesIds: [...state.repliesIds, ...action.payload],
-            };
-        case GET_REPLY:
-            return {
-                ...state, replies: [...state.replies, action.payload],
-            };
-        case LOAD_MORE:
-            let pageSize = state.pageSize + PAGE_SIZE
-            pageSize = pageSize > state.commentsIds.length ? state.commentsIds.length : pageSize
-            const addFrom = state.currentCommentsIds.length
-            const addTo = pageSize
-            return {
-                ...state,
-                pageSize,
-                currentCommentsIds: [...state.currentCommentsIds, ...state.commentsIds.slice(addFrom, addTo)],
             };
         default:
             return state;
